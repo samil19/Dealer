edfe18014a41424f3eb57ff7eaf3cd7ea3336a53
samil19 <samil.ibm@outlook.com>
2018-03-05 22:50:55 -0400

    Error en el AutoDetails, Edit e Index2 con las fotos, y paginas de exeptions

---
+++ Dealer/Controllers/ErrorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dealer.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Error");
+        }
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;  //you may want to set this to 200
+            return View("NotFound");
+        }
+        public ViewResult Unavailable()
+        {
+            Response.StatusCode = 503;
+            return View("Unavailable");
+        }
+    }
+}
